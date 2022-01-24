aef281b85ba91fe909ef5ffcb51fe8f82aa9ebe0
vipinchandraarunkumar <94529727+vipinchandraarunkumar@users.noreply.github.com>
2022-01-24 15:45:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ function my_mousemove(e)
         console.log("current position of x and y coordinates=");
         console.log("x="+current_position_of_x+"y="+current_position_of_y);
         ctx.lineTo(current_position_of_x, current_position_of_y);
+        ctx.stroke();
     }
     last_position_of_x=current_position_of_x;
     last_position_of_y=current_position_of_y;
